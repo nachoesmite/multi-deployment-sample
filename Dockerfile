4bde132c57070afdd41220cb05ec8773f07f1c7c
Ignacio Esmite <iesmite@gmail.com>
2018-10-30 14:00:21 -0300

    add mock endpoints

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM node:8
-# Create app directory
-WORKDIR /usr/src/app
-
-COPY package*.json ./
-
-RUN npm install
+FROM mhart/alpine-node:8 as base
+WORKDIR /usr/src
+COPY package*.json /usr/src/
+RUN npm install --production
 COPY . .
 
-EXPOSE 3000
-CMD [ "npm", "start" ]
+FROM mhart/alpine-node:base-8
+WORKDIR /usr/src
+ENV NODE_ENV="production"
+COPY --from=base /usr/src .
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const port = 3000;
 
+app.use(bodyParser.json({ limit: '500k', extended: true }));
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/:service/:servicePath', (req, res) => {
+  const result = Object.assign({}, { body: req.body }, req.params);
+  res.json(result);
+});
+app.post('/fail/:service/:servicePath', (req, res) => res.status(500).json(Object.assign({ status: 'ERROR' }, { body: req.body }, req.params)));
+
+app.get('/payload', (req,res) => res.json(req.body));
+
+app.get('/endpoints', (req, res) => res.json(
+  ['POST:/:service/:servicePath', 'POST:/fail/:service/:servicePath', 'GET:/payload']
+));
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
